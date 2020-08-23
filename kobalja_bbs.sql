fbbb39124ab8dd18cecc4e3f53072b3f73b537d8
Kohansol <64995062+khs-st@users.noreply.github.com>
2020-08-23 13:55:02 +0900

    게시판 데이터베이스 추가
    게시판 11개 글 작성 데이터 추가

---
+++ kobalja_bbs.sql
@@ -0,0 +1,55 @@
+-- MySQL dump 10.13  Distrib 8.0.21, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: kobalja
+-- ------------------------------------------------------
+-- Server version	8.0.21
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `bbs`
+--
+
+DROP TABLE IF EXISTS `bbs`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `bbs` (
+  `bbsID` int NOT NULL,
+  `bbsTitle` varchar(50) DEFAULT NULL,
+  `userID` varchar(20) DEFAULT NULL,
+  `bbsDate` datetime DEFAULT NULL,
+  `bbsContent` varchar(2048) DEFAULT NULL,
+  `bbsAvailable` int DEFAULT NULL,
+  PRIMARY KEY (`bbsID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `bbs`
+--
+
+LOCK TABLES `bbs` WRITE;
+/*!40000 ALTER TABLE `bbs` DISABLE KEYS */;
+INSERT INTO `bbs` VALUES (1,'test','khs','2020-08-23 12:44:13','test',1),(2,'test2','khs','2020-08-23 13:22:14','test2',1),(3,'글쓰기 출력 화면까지 구현!(게시글 내용은 볼 수 없다.)','khs','2020-08-23 13:27:56','하지만 게시글 내용은 못본다.',1),(4,'4','khs','2020-08-23 13:36:05','4',1),(5,'5','khs','2020-08-23 13:36:09','5',1),(6,'6','khs','2020-08-23 13:36:14','6',1),(7,'7','khs','2020-08-23 13:36:18','7',1),(8,'8','khs','2020-08-23 13:36:21','8',1),(9,'9','khs','2020-08-23 13:36:25','9',1),(10,'10','khs','2020-08-23 13:36:30','10',1),(11,'다음페이지로 넘어감 nextPage(pageNumber+1)','khs','2020-08-23 13:37:00','다음페이지로! ',1);
+/*!40000 ALTER TABLE `bbs` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-08-23 13:53:25
